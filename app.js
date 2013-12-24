daf4f8f4184c5ee756aee21f4950da91af8561f2
Trevor <trevorjbaron@gmail.com>
2013-12-24 04:07:05 +0000

    add www.chatleap

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var httpProxy = require('http-proxy');
 
 var server = httpProxy.createServer({
    router: {
-     '127.0.0.1': 'localhost:3001',
+     '192.241.174.150': 'localhost:3001',
+     'chatleap.com': 'localhost:3001',
+     'www.chatleap.com': 'localhost:3001',
      '.*': 'localhost:3000'
    }
 });
