f694807f50079e3e9a24ff844da72d316cafe1b7
Trevor <trevorjbaron@gmail.com>
2014-01-04 23:01:22 +0000

    fix

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var httpProxy = require('http-proxy');
 
 var server = httpProxy.createServer({
    router: {
-     '192.241.174.150': 'localhost:3001',
+     '192.241.174.150': 'localhost:3002',
+     'trevordev.com': 'localhost:3003',
+     'www.trevordev.com': 'localhost:3003',
+     'jokeydoke.com': 'localhost:3002',
+     'www.jokeydoke.com': 'localhost:3002',
      'chatleap.com': 'localhost:3001',
      'www.chatleap.com': 'localhost:3001',
      '.*': 'localhost:3000'
